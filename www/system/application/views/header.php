2140b7a9af0861cf7497dea99492e2e2982c9aa5
Joe Cheng <joe@joecheng.com>
2010-04-05 11:36:42 -0700

    No YUI for application (yet)

--- www/system/application/views/header.php
+++ www/system/application/views/header.php
@@ -6,7 +6,7 @@
   <script type="text/javascript" src="<?php echo base_url(); ?>static/js/jquery-1.3.2.min.js"></script>
   <script type="text/javascript" src="<?php echo base_url(); ?>static/js/jquery.color.js"></script>
   <script type="text/javascript" src="<?php echo base_url(); ?>static/js/json2.js"></script>
-  <?php $this->load->view('yui'); ?>
+  <?php //$this->load->view('yui'); ?>
 </head>
 <body>
 <div id="pageframe">
